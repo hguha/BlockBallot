8e455837140144676c9c24b7329e9e7e05d077f9
Benjamin Streit <benjaminmstreit@gmail.com>
2019-10-23 17:48:02 -0500

    Initializing react application.

---
+++ elect-wise/src/components/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import '../styles/App.css';
+
+function App() {
+  return (
+    <>
+      <div>
+        I'm the App component.
+      </div>
+      <div>
+        I will consume other components that make up this application, such as a VoterRegistration
+        component or a AvailablePolls component.
+      </div>
+    </>
+  );
+}
+
+export default App;
